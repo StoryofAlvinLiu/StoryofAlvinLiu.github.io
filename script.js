12e1229da30679240eee29e123defcceb6e8f0ec
Alvin Liu <107652281+StoryofAlvinLiu@users.noreply.github.com>
2024-11-21 18:10:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-//DEDfdsafsadfsd
-
-var name = "ALVIN LIU";
-
-function LinkedIn() {
-  window.open("https://profilelandingpage.alvinliusa.repl.co/")
-  console.log("User was redirected to Linkedin")
-}
-
-function EMAILME() {
-  window.open("mailto:alvinliusa@gmail.com")
-  console.log("User was redirected to email Alvin")
-}
+// JavaScript can be added for interactive elements if needed
