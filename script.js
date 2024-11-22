fb339a0374b04f78941e9566c6f32c7e2ca1addb
Alvin Liu <107652281+StoryofAlvinLiu@users.noreply.github.com>
2024-11-21 17:30:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function toggleMenu() {
-    const navbar = document.getElementById("navbar");
-    navbar.style.display = navbar.style.display === "block" ? "none" : "block";
+//DEDfdsafsadfsd
+
+var name = "ALVIN LIU";
+
+function LinkedIn() {
+  window.open("https://profilelandingpage.alvinliusa.repl.co/")
+  console.log("User was redirected to Linkedin")
+}
+
+function EMAILME() {
+  window.open("mailto:alvinliusa@gmail.com")
+  console.log("User was redirected to email Alvin")
 }
